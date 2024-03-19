98fc3676248cc25ce2d15f09b1c6c19ee8ad71dc
Bobby Figgs <rcfiggs@gmail.com>
2024-03-19 15:53:30 -0700

    List with increment values click

--- MyApp.scala
+++ MyApp.scala
@@ -5,15 +5,24 @@ import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.scene.control.{Button, ListView}
 
-case class MyObject(name: String)
+case class MyObject(name: String, var value: Int) {
+  def incrementValue(): Unit = {
+    value += 1
+  }
+}
 
 object MyApp extends JFXApp3 {
   override def start(): Unit = {
     val listView = new ListView[String]()
-    val myObjects = List(MyObject("Item 1"), MyObject("Item 2"), MyObject("Item 3"), MyObject("Item 4"), MyObject("Item 5"))
-    listView.getItems().addAll(myObjects.map(_.name).asJava)
+    val myObjects = List(MyObject("Item 1", 1), MyObject("Item 2", 2), MyObject("Item 3", 3), MyObject("Item 4", 4), MyObject("Item 5", 5))
+    listView.getItems().addAll(myObjects.map(obj => s"${obj.name}: ${obj.value}").asJava)
+
     listView.onMouseClicked = (event) => {
-      println(s"Item ${listView.getSelectionModel().getSelectedIndex()} clicked!")
+      val selectedIndex = listView.getSelectionModel().getSelectedIndex()
+      if (selectedIndex != -1) {
+        myObjects(selectedIndex).incrementValue()
+        listView.getItems().set(selectedIndex, s"${myObjects(selectedIndex).name}: ${myObjects(selectedIndex).value}")
+      }
     }
 
     val scene = new Scene(listView, 300, 200)
