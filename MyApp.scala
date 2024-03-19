ce0d3c9846a906b711a2b9bb619cdf0b18fd611b
Bobby Figgs <rcfiggs@gmail.com>
2024-03-19 10:20:44 -0700

    Simple button grid

--- MyApp.scala
+++ MyApp.scala
@@ -2,18 +2,28 @@ import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.scene.control.Button
+import scalafx.scene.layout.GridPane
 
 
 object MyApp extends JFXApp3 {
   override def start(): Unit = {
-    val button = new Button("Click me!")
-    button.onAction = (event) => {
-      println("Button clicked!")
+    val grid = new GridPane()
+    grid.hgap = 10
+    grid.vgap = 10
+
+    for (i <- 0 to 4) {
+      for (j <- 0 to 4) {
+        val button = new Button(s"Button $i,$j")
+        button.onAction = (event) => {
+          println(s"Button $i,$j clicked!")
+        }
+        grid.add(button, i, j)
+      }
     }
 
-    val scene = new Scene(button, 300, 200)
+    val scene = new Scene(grid, 300, 200)
 
-    stage = PrimaryStage()
+    stage = new PrimaryStage()
     stage.scene = scene
     stage.show()
   }
