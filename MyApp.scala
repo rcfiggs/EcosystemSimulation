748ed9e0ee1d3301c14c5752452802f5bd59146b
Bobby Figgs <rcfiggs@gmail.com>
2024-03-19 15:17:33 -0700

    List of items from object

--- MyApp.scala
+++ MyApp.scala
@@ -1,25 +1,22 @@
+import scalafx.Includes._
+import scala.jdk.CollectionConverters
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.scene.control.{Button, ListView}
-import scalafx.scene.layout.GridPane
 
+case class MyObject(name: String)
 
 object MyApp extends JFXApp3 {
   override def start(): Unit = {
-    val grid = new GridPane()
-    grid.hgap = 10
-    grid.vgap = 10
-
     val listView = new ListView[String]()
-    listView.getItems().addAll("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
+    val myObjects = List(MyObject("Item 1"), MyObject("Item 2"), MyObject("Item 3"), MyObject("Item 4"), MyObject("Item 5"))
+    listView.getItems().addAll(myObjects.map(_.name).asJava)
     listView.onMouseClicked = (event) => {
       println(s"Item ${listView.getSelectionModel().getSelectedIndex()} clicked!")
     }
 
-    grid.add(listView, 0, 0)
-
-    val scene = new Scene(grid, 300, 200)
+    val scene = new Scene(listView, 300, 200)
 
     stage = new PrimaryStage()
     stage.scene = scene
