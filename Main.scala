88db8e9c12fe4079fef9adfe4113dff6aaaa40b1
Bobby Figgs <rcfiggs@gmail.com>
2024-03-20 12:07:29 -0700

    view is not receiving feedback from model

--- Main.scala
+++ Main.scala
@@ -1,133 +1,87 @@
-import scalafx.application.JFXApp3
-import scalafx.scene.Scene
-import scalafx.scene.control.Button
-import scalafx.scene.layout.BorderPane
-import scalafx.scene.canvas.Canvas
-import scalafx.scene.canvas.GraphicsContext
-import scalafx.scene.paint.Color
-import scalafx.application.JFXApp3.PrimaryStage
-
-import scalafx.animation.Timeline
-import scalafx.event.ActionEvent
-import scalafx.util.Duration
-
 import scalafx.Includes._
-import scalafx.animation.KeyFrame
-import scalafx.scene.shape.Rectangle
-import scalafx.scene.control.ContextMenu
-import scalafx.scene.control.MenuItem
+import scala.jdk.CollectionConverters._
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.scene.control.{Button, ListView}
+import scalafx.event.ActionEvent
+import scala.compiletime.uninitialized
 
-case class GameObject(
-  name: String, 
-  value: Int = (Math.random() * 10).toInt,
-  
-){
-  def getActions(): List[String] = List("Print", "Name")
-   def performAction(action: String) = {
-    action match 
-      case "Print" => println("printed")
-      case "Name" => println(name)
+case class MyObject(name: String, var value: Int) {
+  def incrementValue(): Unit = {
+    value += 1
   }
 }
 
-object GameState {
-  var objects: List[GameObject] = List[GameObject]()
-}
+// GameState.scala
+class GameState {
+  private var myObjects: List[MyObject] = List(MyObject("Item 1", 1), MyObject("Item 2", 2), MyObject("Item 3", 3), MyObject("Item 4", 4), MyObject("Item 5", 5))
 
-class GameCanvas(width: Double, height: Double) extends Canvas(width, height) {
-  val gc = graphicsContext2D
-  
-  def drawGreenTriangle(): Unit = {
-    val xPoints = Array[Double](100, 200, 100)
-    val yPoints = Array[Double](100, 200, 200)
-    gc.setFill(Color.Green)
-    gc.fillPolygon(xPoints, yPoints, 3)
+  def incrementValues(): Unit = {
+    println("inc")
+    println(myObjects)
+    myObjects.foreach(_.incrementValue())
   }
-  
-  def renderText(objects: List[GameObject]): Unit = {
-    gc.setFill(Color.Black)
-    gc.setStroke(Color.Blue) // Color for box outline
-    
-    val boxWidth = 100
-    val boxHeight = 30
-    val maxItemsPerColumn = 5
-    val columnSpacing = 20
-    
-    objects.zipWithIndex.foreach { case (obj, index) =>
-      val column = index % maxItemsPerColumn
-      val row = index / maxItemsPerColumn
-      val x = 10 + (column * (boxWidth + 20))
-      val y = 20 + (row * boxHeight)
-      
-      // Draw the box
-      gc.strokeRect(x, y, boxWidth, boxHeight)
-      
-      // Draw the text within the box (adjust offsets as needed)
-      gc.fillText(obj.toString, x + 5, y + 20)
-      
-      // Add a mouse click event handler to the box
-      val box = Rectangle(x, y, boxWidth, boxHeight)
-      box.setOnMouseClicked((event) => {
-        // Display the list of available actions for the GameObject
-        val actions = obj.getActions()
-        val actionMenu = new ContextMenu()
-        actions.foreach((action) => {
-          val menuItem = new MenuItem(action)
-          menuItem.setOnAction((event) => {
-            // Perform the action on the GameObject
-            obj.performAction(action)
-          })
-          actionMenu.getItems.add(menuItem)
-        })
-        actionMenu.show(this, event.getScreenX, event.getScreenY)
-      })
-    }
+
+  def getObjectValues(): List[String] = {
+    myObjects.map(obj => s"${obj.name}: ${obj.value}")
   }
 }
 
-object GameUI extends JFXApp3 {
+// GameView.scala
+class GameView(game: Game) {
+  val listView = new ListView[String]()
+  val nextDayButton = new Button("Next Day")
+
+  nextDayButton.onAction = (_: ActionEvent) => {
+    game.handleNextDay()
+  }
+
+  def start(): Scene = {
+    val scene = new Scene(new javafx.scene.layout.VBox(listView, nextDayButton), 300, 200)
+    update()
+    scene
+  }
+
+  def update(): Unit = {
+    listView.getItems().clear()
+    listView.getItems().addAll(game.getObjectValues().asJava)
+  }
+}
+
+// GameController.scala
+class GameController(game: Game, view: GameView) {
+  def handleNextDay(): Unit = {
+    game.incrementValues()
+    view.update()
+  }
+}
+
+// Game.scala
+class Game {
+  private val state: GameState = new GameState()
+
+  def incrementValues(): Unit = {
+    state.incrementValues()
+  }
+
+  def getObjectValues(): List[String] = {
+    state.getObjectValues()
+  }
+
+  def handleNextDay(): Unit = {
+    incrementValues()
+  }
+}
+
+// Main.scala
+object Main extends JFXApp3 {
+  val game = new Game()
   override def start(): Unit = {
-    val gameCanvas = new GameCanvas(600, 400)
-    GameState.objects = GameState.objects.appendedAll((0 to 20).map((i: Int) => GameObject(i.toString)))
-    gameCanvas.drawGreenTriangle()
-    gameCanvas.renderText(GameState.objects)
-    
-    val buttons = new BorderPane {
-      styleClass += "button-panel"
-      prefHeight = 100
-      children = List(
-      new Button("Button 1"),
-      new Button("Button 2"),
-      new Button("Button 3")
-      )
-    }
-    
-    stage = new PrimaryStage {
-      title = "My ScalaFX Project"
-      scene = new Scene {
-        root = new BorderPane {
-          top = gameCanvas
-          bottom = buttons
-        }
-      }
-    }
-    
-    val timeline = new Timeline {
-      cycleCount = Timeline.Indefinite
-      keyFrames = KeyFrame(Duration(1000), onFinished = (e: ActionEvent) => {
-        // Clear the canvas
-        gameCanvas.gc.clearRect(0, 0, gameCanvas.getWidth, gameCanvas.getHeight)
-        
-        // Get the updated list of game objects (replace with actual logic for updating/fetching game objects)
-        val updatedGameObjects = GameState.objects
-        
-        // Redraw the triangle if needed
-        gameCanvas.drawGreenTriangle()
-        
-        // Render the updated game objects
-        gameCanvas.renderText(updatedGameObjects)
-      })
-    }
-    timeline.play()
+    val view = new GameView(game)
+    val controller = new GameController(game, view)
+    stage = new PrimaryStage()
+    stage.scene = view.start()
+    stage.show()
   }
 }
--- MyApp.scala
+++ MyApp.scala
@@ -6,26 +6,17 @@ import scalafx.scene.Scene
 import scalafx.scene.control.{Button, ListView}
 import scalafx.event.ActionEvent
 
-case class MyObject(name: String, var value: Int) {
-  def incrementValue(): Unit = {
-    value += 1
-  }
-}
+// case class MyObject(name: String, var value: Int) {
+//   def incrementValue(): Unit = {
+//     value += 1
+//   }
+// }
 
 object MyApp extends JFXApp3 {
   override def start(): Unit = {
     val listView = new ListView[String]()
     val myObjects = List(MyObject("Item 1", 1), MyObject("Item 2", 2), MyObject("Item 3", 3), MyObject("Item 4", 4), MyObject("Item 5", 5))
     listView.getItems().addAll(myObjects.map(obj => s"${obj.name}: ${obj.value}").asJava)
-
-    listView.onMouseClicked = (event) => {
-      val selectedIndex = listView.getSelectionModel().getSelectedIndex()
-      if (selectedIndex != -1) {
-        myObjects(selectedIndex).incrementValue()
-        listView.getItems().set(selectedIndex, s"${myObjects(selectedIndex).name}: ${myObjects(selectedIndex).value}")
-        println(s"${myObjects(selectedIndex).name}: ${myObjects(selectedIndex).value}")
-      }
-    }
 
     val nextDayButton = new Button("Next Day")
     nextDayButton.onAction = (_: ActionEvent) => {
