c141dcc07b9d8c7bfb27f082a6942e4cb2c03703
Bobby Figgs <rcfiggs@gmail.com>
2024-05-11 17:36:24 -0700

    Update Perished Organism tests

--- src/test/scala/model/entities/organisms/PerishedOrganismSuite.scala
+++ src/test/scala/model/entities/organisms/PerishedOrganismSuite.scala
@@ -6,28 +6,21 @@ import _root_.model.events.{ExtractResource, IsPerished}
 import _root_.model.dna.{DNA, Extraction, Consumption, Capacity, InitialResource}
 
 class PerishedOrganismSuite extends AnyFunSuite {
-
-  val dna = DNA(properties = Map(
-    Extraction(Water) -> 10,
-    Consumption(Sugar) -> 5,
-    Capacity(Water) -> 50,
-    Capacity(Sugar) -> 50,
-    InitialResource(Water) -> 10,
-  ))
-
+  val dna: DNA = DNA(properties = Map())
+  val resources = scala.collection.mutable.Map[Resource,Int]()
   test("A PerishedOrganism should have the correct DNA") {
-    val perishedOrganism = PerishedOrganism(1, dna)
+    val perishedOrganism = PerishedOrganism(1, resources)
     assert(perishedOrganism.dna === dna)
   }
 
   test("A PerishedOrganism should have the correct initial resources") {
-    val perishedOrganism = PerishedOrganism(1, dna)
-    assert(perishedOrganism.resources === dna.initialResources)
+    val perishedOrganism = PerishedOrganism(1, resources)
+    assert(perishedOrganism.resources === resources)
   }
 
   test("A PerishedOrganism should handle an ExtractResource event from an Animal correctly") {
     val animal = Animal()
-    val perishedOrganism = PerishedOrganism(1, dna)
+    val perishedOrganism = PerishedOrganism(1, scala.collection.mutable.Map[Resource,Int]())
     val event = ExtractResource(targetId = 1, resource = Water, amount = 5, sender = animal)
     val actual = perishedOrganism.eventHandlers(event)
     val expected = Seq(IsPerished(animal.id, perishedOrganism))
